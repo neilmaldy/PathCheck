7f4e8a1685ddc2721bdb4b9396192aed1befcc08
Neil Maldonado <neilmaldy@gmail.com>
2018-01-30 17:59:10 -0500

    Testing basic class functionality

--- aggregate.py
+++ aggregate.py
@@ -1,4 +1,5 @@
 from myLogging import printToLog
+from itertools import combinations
 
 debugit = 2
 
@@ -81,3 +82,165 @@ class Lun:
             return True
         else:
             return False
+
+
+class PathCheckObject:
+    def __init__(self, asup_lines):
+        self.aggregates = []
+
+        path_to_lun = {}
+        lun_to_paths = {}
+        ports = []
+
+        hostname = ''
+        aggregate = None
+
+        for line in asup_lines:
+            if 'System Serial Number:' in line:
+                printToLog('Starting sysconfig-a')
+                hostname = line.split()[-1][1:-1]
+                if debugit > 2:
+                    printToLog(hostname)
+                continue
+            if 'HITACHI' in line:
+                path = hostname + '_' + line.split()[0]
+                port = path.split('_')[1].split('.')[0]
+                if port not in ports:
+                    ports.append(port)
+
+                if debugit > 2:
+                    printToLog(path)
+                lun_name = hostname + '_' + line.split()[-1][1:-1]
+                if debugit > 2:
+                    printToLog(lun_name)
+                if path not in path_to_lun:
+                    path_to_lun[path] = lun_name
+                else:
+                    if lun_name != path_to_lun[path]:
+                        printToLog("Error! path-lun mismatch " + path + ',' + lun_name + ',' + path_to_lun[path])
+                if lun_name not in lun_to_paths:
+                    lun_to_paths[lun_name] = []
+                lun_to_paths[lun_name].append(path)
+                if debugit > 1:
+                    printToLog('Adding ' + path + ' to ' + lun_name)
+                    printToLog(lun_name + ' path count ' + str(len(lun_to_paths[lun_name])))
+                continue
+
+            if 'Aggregate' in line:
+                printToLog('Starting sysconfig-m')
+                aggregate_name = line.split()[1]
+                if debugit > 2:
+                    printToLog(aggregate_name)
+                aggregate = Aggregate(aggregate_name)
+                self.aggregates.append(aggregate)
+                continue
+            if 'Plex' in line:
+                plex_name = line.split()[1]
+                if debugit > 2:
+                    printToLog(plex_name)
+                plex = Plex(plex_name)
+                aggregate.add_plex(plex)
+                continue
+            if 'LUN' in line:
+                path = hostname + '_' + line.split()[1]
+                if path in path_to_lun:
+                    if path_to_lun[path] not in plex.lun_names:
+                        lun = Lun(path_to_lun[path])
+                        if lun.name in lun_to_paths:
+                            lun.set_paths(lun_to_paths[lun.name])
+                        plex.add_lun(lun)
+                    else:
+                        printToLog(
+                            'Warning trying to add ' + path_to_lun[path] + ' to ' + plex.name + ' but already exists')
+
+                if debugit > 2:
+                    printToLog(path)
+                continue
+
+        for aggregate in self.aggregates:
+            for plex in aggregate.plexes:
+                for lun in plex.luns:
+                    for path in lun.paths:
+                        printToLog(','.join(
+                            [aggregate.name, plex.name, lun.name, path, lun.name.split('_')[1], path.split('_')[1],
+                             path.split('_')[1].split('.')[0]]))
+
+    def check(self, down_ports=None):
+        offline_aggregates = []
+        offline_plexes = []
+
+        for aggregate in self.aggregates:
+            for plex in aggregate.plexes:
+                if plex.is_online(down_ports):
+                    printToLog(plex.name + ' plex is online')
+                else:
+                    printToLog(plex.name + ' plex is offline')
+                    offline_plexes.append(plex.name)
+
+            if aggregate.is_offline(down_ports):
+                printToLog(aggregate.name + ' aggr is offline')
+                offline_aggregates.append(aggregate.name)
+            else:
+                printToLog(aggregate.name + ' aggr is online')
+
+        return (offline_aggregates, offline_plexes)
+
+        down_ports_combination = ['switch1:1', 'switch2:1']
+        printToLog('down_ports_combination=' + ' '.join(down_ports_combination))
+        for aggregate in self.aggregates:
+            for plex in aggregate.plexes:
+                if plex.is_online(down_ports_combination):
+                    printToLog(plex.name + ' plex is online')
+                else:
+                    printToLog(plex.name + ' plex is offline')
+
+            if aggregate.is_offline(down_ports_combination):
+                printToLog(aggregate.name + ' aggr is offline')
+            else:
+                printToLog(aggregate.name + ' aggr is online')
+
+        down_ports_combination = ["nys42ab11:24", "nys42ab11:22", "nys42ab11:23", "nys42ab11:25"]
+        printToLog('down_ports_combination=' + ' '.join(down_ports_combination))
+        for aggregate in self.aggregates:
+            for plex in aggregate.plexes:
+                if plex.is_online(down_ports_combination):
+                    printToLog(plex.name + ' plex is online')
+                else:
+                    printToLog(plex.name + ' plex is offline')
+
+            if aggregate.is_offline(down_ports_combination):
+                printToLog(aggregate.name + ' aggr is offline')
+            else:
+                printToLog(aggregate.name + ' aggr is online')
+
+        down_ports_combination = ["nys42bb1:24", "nys42bb1:23", "nys42bb1:25", "nys42bb1:22"]
+        printToLog('down_ports_combination=' + ' '.join(down_ports_combination))
+        for aggregate in self.aggregates:
+            for plex in aggregate.plexes:
+                if plex.is_online(down_ports_combination):
+                    printToLog(plex.name + ' plex is online')
+                else:
+                    printToLog(plex.name + ' plex is offline')
+
+            if aggregate.is_offline(down_ports_combination):
+                printToLog(aggregate.name + ' aggr is offline')
+            else:
+                printToLog(aggregate.name + ' aggr is online')
+
+        down_ports_combination = ["nys42bb1:24", "nys42bb1:23", "nys42bb1:25", "nys42bb1:22"]
+        printToLog('down_ports_combination=' + ' '.join(down_ports_combination))
+        for aggregate in self.aggregates:
+            for plex in aggregate.plexes:
+                if plex.is_online(down_ports_combination):
+                    printToLog(plex.name + ' plex is online')
+                else:
+                    printToLog(plex.name + ' plex is offline')
+
+            if aggregate.is_offline(down_ports_combination):
+                printToLog(aggregate.name + ' aggr is offline')
+            else:
+                printToLog(aggregate.name + ' aggr is online')
+
+        # printToLog('["' + '","'.join(ports) + '"]'
+
+
--- test_aggregate.py
+++ test_aggregate.py
@@ -1,5 +1,5 @@
-from aggregate import Aggregate, Plex, Lun
-from pytest import raises
+from aggregate import Aggregate, Plex, Lun, PathCheckObject
+from pytest import raises, fixture
 
 
 def test_create_lun():
@@ -151,3 +151,121 @@ def test_aggregate_is_offline_true():
     aggregate.add_plex(plex=plex2)
 
     assert aggregate.is_offline(down_ports=['port1', 'port2', 'port4']) is True
+
+
+@fixture(scope="module")
+def path_check_object():
+    with open('testasups.txt') as fp:
+        asup_lines = fp.read().splitlines()
+
+    return PathCheckObject(asup_lines=asup_lines)
+
+
+def test_create_path_check_object_aggregates(path_check_object):
+    # check aggr names
+    assert (set([aggr.name for aggr in path_check_object.aggregates]) == {'host1a1', 'host1a2', 'host1a0', 'host2a0', 'host2a1'}) is True
+
+
+def test_create_path_check_object_plexes(path_check_object):
+    # check plex names
+    for aggr in path_check_object.aggregates:
+        if aggr.name == 'host1a1':
+            assert (set(aggr.plex_names) == {'/host1a1/plex0', '/host1a1/plex2'}) is True
+        elif aggr.name == 'host1a2':
+            assert (set(aggr.plex_names) == {'/host1a2/plex0', '/host1a2/plex2'}) is True
+        elif aggr.name == 'host1a0':
+            assert (set(aggr.plex_names) == {'/host1a0/plex0', '/host1a0/plex2'}) is True
+        elif aggr.name == 'host2a0':
+            assert (set(aggr.plex_names) == {'/host2a0/plex0', '/host2a0/plex2'}) is True
+        elif aggr.name == 'host2a1':
+            assert (set(aggr.plex_names) == {'/host2a1/plex0', '/host2a1/plex2'}) is True
+
+
+def test_create_path_check_object_luns(path_check_object):
+    # check lun names
+    for aggr in path_check_object.aggregates:
+        for plex in aggr.plexes:
+            if plex.name == '/host1a1/plex0':
+                assert (set(plex.lun_names) == {'hostname1_930425000001', 'hostname1_930425000002'}) is True
+            elif plex.name == '/host1a1/plex2':
+                assert (set(plex.lun_names) == {'hostname1_930425000003', 'hostname1_930425000004'}) is True
+            elif plex.name == '/host1a2/plex0':
+                assert (set(plex.lun_names) == {'hostname1_930425000005', 'hostname1_930425000006'}) is True
+            elif plex.name == '/host1a2/plex2':
+                assert (set(plex.lun_names) == {'hostname1_930425000007', 'hostname1_930425000008'}) is True
+            elif plex.name == '/host1a0/plex0':
+                assert (set(plex.lun_names) == {'hostname1_930425000009'}) is True
+            elif plex.name == '/host1a0/plex2':
+                assert (set(plex.lun_names) == {'hostname1_93042500000A'}) is True
+            elif plex.name == '/host2a0/plex0':
+                assert (set(plex.lun_names) == {'hostname2_930425000011'}) is True
+            elif plex.name == '/host1a1/plex0':
+                assert (set(plex.lun_names) == {'hostname2_930425000012'}) is True
+            elif plex.name == '/host1a1/plex0':
+                assert (set(plex.lun_names) == {'hostname2_930425000013', 'hostname2_930425000014'}) is True
+            elif plex.name == '/host1a1/plex0':
+                assert (set(plex.lun_names) == {'hostname2_930425000015', 'hostname2_930425000016'}) is True
+
+
+def test_create_path_check_object_paths_ports(path_check_object):
+
+    # check paths and ports
+    paths = dict()
+    paths['hostname1_930425000001'] = ['hostname1_switch1:1.126L1', 'hostname1_switch2:1.126L1']
+    paths['hostname1_930425000002'] = ['hostname1_switch1:2.126L2', 'hostname1_switch2:2.126L2']
+    paths['hostname1_930425000003'] = ['hostname1_switch1:3.126L3', 'hostname1_switch2:3.126L3']
+    paths['hostname1_930425000004'] = ['hostname1_switch1:4.126L4', 'hostname1_switch2:4.126L4']
+    paths['hostname1_930425000005'] = ['hostname1_switch1:5.126L5', 'hostname1_switch2:5.126L5']
+    paths['hostname1_930425000006'] = ['hostname1_switch1:6.126L6', 'hostname1_switch2:6.126L6']
+    paths['hostname1_930425000007'] = ['hostname1_switch1:7.126L7', 'hostname1_switch2:7.126L7']
+    paths['hostname1_930425000008'] = ['hostname1_switch1:8.126L8', 'hostname1_switch2:8.126L8']
+    paths['hostname1_930425000009'] = ['hostname1_switch1:9.126L9', 'hostname1_switch2:9.126L9']
+    paths['hostname1_93042500000A'] = ['hostname1_switch1:0.126L0', 'hostname1_switch2:0.126L0']
+    paths['hostname2_930425000011'] = ['hostname2_switch1:10.126L11', 'hostname2_switch2:10.126L11']
+    paths['hostname2_930425000012'] = ['hostname2_switch1:10.126L12', 'hostname2_switch2:10.126L12']
+    paths['hostname2_930425000013'] = ['hostname2_switch1:10.126L13', 'hostname2_switch2:10.126L13']
+    paths['hostname2_930425000014'] = ['hostname2_switch1:10.126L14', 'hostname2_switch2:10.126L14']
+    paths['hostname2_930425000015'] = ['hostname2_switch1:10.126L15', 'hostname2_switch2:10.126L15']
+    paths['hostname2_930425000016'] = ['hostname2_switch1:20.126L16', 'hostname2_switch2:20.126L16']
+    ports = dict()
+    ports['hostname1_930425000001'] = ['switch1:1', 'switch2:1']
+    ports['hostname1_930425000002'] = ['switch1:2', 'switch2:2']
+    ports['hostname1_930425000003'] = ['switch1:3', 'switch2:3']
+    ports['hostname1_930425000004'] = ['switch1:4', 'switch2:4']
+    ports['hostname1_930425000005'] = ['switch1:5', 'switch2:5']
+    ports['hostname1_930425000006'] = ['switch1:6', 'switch2:6']
+    ports['hostname1_930425000007'] = ['switch1:7', 'switch2:7']
+    ports['hostname1_930425000008'] = ['switch1:8', 'switch2:8']
+    ports['hostname1_930425000009'] = ['switch1:9', 'switch2:9']
+    ports['hostname1_93042500000A'] = ['switch1:0', 'switch2:0']
+    ports['hostname2_930425000011'] = ['switch1:10', 'switch2:10']
+    ports['hostname2_930425000012'] = ['switch1:10', 'switch2:10']
+    ports['hostname2_930425000013'] = ['switch1:10', 'switch2:10']
+    ports['hostname2_930425000014'] = ['switch1:10', 'switch2:10']
+    ports['hostname2_930425000015'] = ['switch1:10', 'switch2:10']
+    ports['hostname2_930425000016'] = ['switch1:20', 'switch2:20']
+
+    for aggr in path_check_object.aggregates:
+        for plex in aggr.plexes:
+            for lun in plex.luns:
+                if lun.name in paths:
+                    assert (set(lun.paths) == set(paths[lun.name])) is True
+                if lun.name in ports:
+                    assert (set(lun.switch_ports) == set(ports[lun.name])) is True
+
+
+def test_plex_offline(path_check_object):
+    offline_aggregates, offline_plexes = path_check_object.check(down_ports=['switch1:1', 'switch2:1'])
+    assert (set(offline_plexes) == {'/host1a1/plex0'}) is True
+    offline_aggregates, offline_plexes = path_check_object.check(down_ports=['switch1:2', 'switch2:1'])
+    assert (set(offline_plexes) == {'/host1a1/plex0'}) is False
+    offline_aggregates, offline_plexes = path_check_object.check(down_ports=['switch1:1', 'switch2:1', 'switch1:2', 'switch2:2'])
+    assert (set(offline_plexes) == {'/host1a1/plex0', '/host1a1/plex0'}) is True
+
+
+def test_aggregate_offline(path_check_object):
+    offline_aggregates, offline_plexes = path_check_object.check(down_ports=['switch1:1', 'switch2:1'])
+    assert (set(offline_aggregates) == {'host1a1'}) is False
+    offline_aggregates, offline_plexes = path_check_object.check(down_ports=['switch1:1', 'switch2:1', 'switch1:3', 'switch2:3'])
+    assert (set(offline_aggregates) == {'host1a1'}) is True
+
