f8b337d556cd21835f0c143c02c27f8fd8ff896d
Neil Maldonado <neilmaldy@gmail.com>
2018-01-31 12:36:29 -0500

    Improving csv output

--- path_check.py
+++ path_check.py
@@ -12,98 +12,102 @@ pco = PathCheckObject(asup_lines=asup_lines)
 port_list = ['nys42ab1:24', 'nys42ab1:25', 'nys42ab1:22', 'nys42ab1:23', 'nys42ab11:24', 'nys42ab11:22', 'nys42ab11:23', 'nys42ab11:25', 'nys42bb1:24',
              'nys42bb1:23', 'nys42bb1:25', 'nys42bb1:22', 'nys42bb11:22', 'nys42bb11:23', 'nys42bb11:25', 'nys42bb11:24']
 
-for r in range(3):
-    for port_combination in combinations(port_list, r):
-        print('\nDown ports = ' + ' '.join(port_combination))
-        offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
+if False:
+    for r in range(3):
+        for port_combination in combinations(port_list, r):
+            print('\nDown ports = ' + ' '.join(port_combination))
+            offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
 
-        if offline_aggregates:
-            print('\nOffline Aggregates:')
-            for aggr in offline_aggregates:
-                print(aggr)
-        else:
-            print('\nAll Aggregates online')
+            if offline_aggregates:
+                print('\nOffline Aggregates:')
+                for aggr in offline_aggregates:
+                    print(aggr)
+            else:
+                print('\nAll Aggregates online')
 
-        if offline_plexes:
-            print('\nOffline Plexes:')
-            for plex in offline_plexes:
-                print(plex)
-        else:
-            print('\nAll Plexes online')
+            if offline_plexes:
+                print('\nOffline Plexes:')
+                for plex in offline_plexes:
+                    print(plex)
+            else:
+                print('\nAll Plexes online')
 
-port_combination = ['nys42ab1:24', 'nys42ab1:25', 'nys42ab1:22', 'nys42ab1:23']
+port_list = ['nys42ab1:24', 'nys42ab1:25', 'nys42ab1:22', 'nys42ab1:23']
+for port_combination in combinations(port_list, 3):
+    print('\nDown ports = ' + ' '.join(port_combination))
+    offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
 
-print('\nDown ports = ' + ' '.join(port_combination))
-offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
+    if offline_aggregates:
+        print('\nOffline Aggregates:')
+        for aggr in offline_aggregates:
+            print(aggr)
+    else:
+        print('\nAll Aggregates online')
 
-if offline_aggregates:
-    print('\nOffline Aggregates:')
-    for aggr in offline_aggregates:
-        print(aggr)
-else:
-    print('\nAll Aggregates online')
+    if offline_plexes:
+        print('\nOffline Plexes:')
+        for plex in offline_plexes:
+            print(plex)
+    else:
+        print('\nAll Plexes online')
 
-if offline_plexes:
-    print('\nOffline Plexes:')
-    for plex in offline_plexes:
-        print(plex)
-else:
-    print('\nAll Plexes online')
+port_list = ['nys42ab11:24', 'nys42ab11:22', 'nys42ab11:23', 'nys42ab11:25']
+for port_combination in combinations(port_list, 3):
 
-port_combination = ['nys42ab11:24', 'nys42ab11:22', 'nys42ab11:23', 'nys42ab11:25']
+    print('\nDown ports = ' + ' '.join(port_combination))
+    offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
 
-print('\nDown ports = ' + ' '.join(port_combination))
-offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
+    if offline_aggregates:
+        print('\nOffline Aggregates:')
+        for aggr in offline_aggregates:
+            print(aggr)
+    else:
+        print('\nAll Aggregates online')
 
-if offline_aggregates:
-    print('\nOffline Aggregates:')
-    for aggr in offline_aggregates:
-        print(aggr)
-else:
-    print('\nAll Aggregates online')
+    if offline_plexes:
+        print('\nOffline Plexes:')
+        for plex in offline_plexes:
+            print(plex)
+    else:
+        print('\nAll Plexes online')
 
-if offline_plexes:
-    print('\nOffline Plexes:')
-    for plex in offline_plexes:
-        print(plex)
-else:
-    print('\nAll Plexes online')
+port_list = ['nys42bb1:24', 'nys42bb1:23', 'nys42bb1:25', 'nys42bb1:22']
+for port_combination in combinations(port_list, 3):
 
-port_combination = ['nys42bb1:24', 'nys42bb1:23', 'nys42bb1:25', 'nys42bb1:22']
+    print('\nDown ports = ' + ' '.join(port_combination))
+    offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
 
-print('\nDown ports = ' + ' '.join(port_combination))
-offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
+    if offline_aggregates:
+        print('\nOffline Aggregates:')
+        for aggr in offline_aggregates:
+            print(aggr)
+    else:
+        print('\nAll Aggregates online')
 
-if offline_aggregates:
-    print('\nOffline Aggregates:')
-    for aggr in offline_aggregates:
-        print(aggr)
-else:
-    print('\nAll Aggregates online')
+    if offline_plexes:
+        print('\nOffline Plexes:')
+        for plex in offline_plexes:
+            print(plex)
+    else:
+        print('\nAll Plexes online')
 
-if offline_plexes:
-    print('\nOffline Plexes:')
-    for plex in offline_plexes:
-        print(plex)
-else:
-    print('\nAll Plexes online')
+port_list = ['nys42bb11:24', 'nys42bb11:23', 'nys42bb11:25', 'nys42bb11:24']
+for port_combination in combinations(port_list, 3):
 
-port_combination = ['nys42bb11:22', 'nys42bb11:23', 'nys42bb11:25', 'nys42bb11:24']
+    print('\nDown ports = ' + ' '.join(port_combination))
+    offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
 
-print('\nDown ports = ' + ' '.join(port_combination))
-offline_aggregates, offline_plexes = pco.check(down_ports=port_combination)
+    if offline_aggregates:
+        print('\nOffline Aggregates:')
+        for aggr in offline_aggregates:
+            print(aggr)
+    else:
+        print('\nAll Aggregates online')
 
-if offline_aggregates:
-    print('\nOffline Aggregates:')
-    for aggr in offline_aggregates:
-        print(aggr)
-else:
-    print('\nAll Aggregates online')
-
-if offline_plexes:
-    print('\nOffline Plexes:')
-    for plex in offline_plexes:
-        print(plex)
-else:
-    print('\nAll Plexes online')
+    if offline_plexes:
+        print('\nOffline Plexes:')
+        for plex in offline_plexes:
+            print(plex)
+    else:
+        print('\nAll Plexes online')
 printToLog('Done!')
